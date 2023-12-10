7dbde669816501be92caa349cd4b16842c9762f3
118design <74572170+118design@users.noreply.github.com>
2023-12-09 19:40:36 -0900

    Initial Commit

---
+++ TrailReport/TrailReportApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrailReportApp.swift
+//  TrailReport
+//
+//  Created by Braden Becker on 12/9/23.
+//
+
+import SwiftUI
+
+@main
+struct TrailReportApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
